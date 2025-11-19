597056f274690037dce1a3f629ef87747a6d11b4
Wise Munene Mutuku <wisemunene99@gmail.com>
2025-11-19 12:14:07 +0300

    done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,17 @@
 def write_file(file_name, file_content):
-    pass
+    path= f"{file_name}.txt"
+    with open (path,'w') as file:
+        file.write(file_content)
+
+
+
 
 def append_file(file_name, append_content):
-    pass
+    path=f"{file_name}.txt"
+    with open (path, "a") as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    path=f"{file_name}.txt"
+    with open(path, "r") as file:
+        return file.read()
